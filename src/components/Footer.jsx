ca418f051a353c116c5d878f18396ecf0062f75b
Kier Joshua <kierjoshuadu1226@gmail.com>
2025-02-24 16:54:56 +0800

    remove animation on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='font-semibold w-full flex flex-col justify-between py-5 sm:py-7 md:py-8 px-8 sm:px-16 md:px-20 bg-[#173558] drop-shadow-sm'>
-        <h1 className='text-center w-full' data-aos="fade-left" data-aos-duration='1000'>&copy; Kier Joshua E. Du  <br /></h1>
-        <h1 className='font-medium text-center w-full' data-aos="fade-right" data-aos-duration='1000'>All Rights Reserved.</h1>
+        <h1 className='text-center w-full'>&copy; Kier Joshua E. Du  <br /></h1>
+        <h1 className='font-medium text-center w-full'>All Rights Reserved.</h1>
     </div>
   )
 }
