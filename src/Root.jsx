6f337848f92c2ca84d6077c335e377e110bc96c4
Kier Joshua <kierjoshuadu1226@gmail.com>
2025-06-23 19:03:49 +0800

    added a scroll to top for changing pages

--- src/Root.jsx
+++ src/Root.jsx
@@ -3,10 +3,12 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import { Outlet } from 'react-router-dom'
 import Socials from './components/Socials'
+import ScrollTop from './components/ScrollTop'
 
 const KRoot = () => {
   return (
     <div className='bg-custom-gradient flex flex-col text-white font-poppins w-full overflow-hidden'>
+      <ScrollTop />
       <Header />
       <Socials />
       <Outlet />
